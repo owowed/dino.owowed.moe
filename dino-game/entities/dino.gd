18ebfaae9b57bfbc8e29b394454fa37171ce83dd
owowed <island@owowed.moe>
2023-05-18 18:36:45 +0700

    added flipped direction when dino is going left
    Signed-off-by: owowed <island@owowed.moe>

--- dino-game/entities/dino.gd
+++ dino-game/entities/dino.gd
@@ -17,6 +17,11 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 		
+		if direction < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+		
 		if Input.is_action_pressed("player.crouch"):
 			$AnimatedSprite2D.play("crouching")
 		else:
@@ -31,3 +36,4 @@ func _physics_process(delta):
 			$AnimatedSprite2D.play("idling")
 			
 	move_and_slide()
+
