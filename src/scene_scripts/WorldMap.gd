b38f00b0cc834b22598b7e05a79a213aaf4d5e13
owowed <island@owowed.moe>
2025-12-18 01:08:59 +0700

    level template for granually creating more levels

--- src/scene_scripts/WorldMap.gd
+++ src/scene_scripts/WorldMap.gd
@@ -3,13 +3,20 @@ extends Node2D
 @export var world_player: WorldPlayer
 @export var level_label: Label
 @export var prompt_label: Label
+@export var prompt_dialog: ConfirmationDialog
 @export var map_bounds := Rect2(-480, -270, 960, 540)
 
 var _active_node: WorldLevelNode
+var _pending_node: WorldLevelNode
 
 func _ready() -> void:
 	if world_player == null:
 		world_player = $WorldPlayer
+	if prompt_dialog == null and has_node("UI/LevelPrompt"):
+		prompt_dialog = $UI/LevelPrompt
+	if prompt_dialog:
+		prompt_dialog.confirmed.connect(_on_prompt_confirmed)
+		prompt_dialog.canceled.connect(_on_prompt_closed)
 	for node in get_tree().get_nodes_in_group("world_level_node"):
 		if node is WorldLevelNode:
 			node.focus_started.connect(_on_node_focus)
@@ -19,7 +26,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	_clamp_player()
 	if _active_node and Input.is_action_just_pressed("world.interact"):
-		_enter_level(_active_node)
+		_show_prompt(_active_node)
 
 func _clamp_player() -> void:
 	if world_player == null:
@@ -35,12 +42,34 @@ func _on_node_focus(node: WorldLevelNode) -> void:
 func _on_node_unfocus(node: WorldLevelNode) -> void:
 	if node == _active_node:
 		_active_node = null
+	if node == _pending_node:
+		_pending_node = null
+		if prompt_dialog:
+			prompt_dialog.hide()
 	_update_ui()
 
 func _enter_level(node: WorldLevelNode) -> void:
 	if not node.is_available():
 		return
 	get_tree().change_scene_to_file(node.level_scene)
+
+func _show_prompt(node: WorldLevelNode) -> void:
+	if not node.is_available():
+		return
+	if prompt_dialog:
+		_pending_node = node
+		prompt_dialog.dialog_text = "Play %s?" % node.level_name
+		prompt_dialog.popup_centered()
+	else:
+		_enter_level(node)
+
+func _on_prompt_confirmed() -> void:
+	if _pending_node:
+		_enter_level(_pending_node)
+	_pending_node = null
+
+func _on_prompt_closed() -> void:
+	_pending_node = null
 
 func _update_ui() -> void:
 	if level_label:
@@ -50,7 +79,7 @@ func _update_ui() -> void:
 			prompt_label.visible = false
 		elif _active_node.is_available():
 			prompt_label.visible = true
-			prompt_label.text = "Press Space/Enter to play"
+			prompt_label.text = "Press Space/Enter to choose"
 		else:
 			prompt_label.visible = true
 			prompt_label.text = "Locked"
